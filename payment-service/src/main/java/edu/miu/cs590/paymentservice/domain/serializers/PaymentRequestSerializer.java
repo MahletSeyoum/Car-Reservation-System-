6ef8199a54f56886371360a2e39f47ebec27c482
MilkaElias <milkaelias23@gmail.com>
2022-06-13 22:24:18 -0500

    Implemented Kafka on the payment-service.

---
+++ payment-service/src/main/java/edu/miu/cs590/paymentservice/domain/serializers/PaymentRequestSerializer.java
@@ -0,0 +1,35 @@
+package edu.miu.cs590.paymentservice.domain.serializers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class PaymentRequestSerializer implements Serializer {
+    @Override
+    public void configure(Map configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String s, Object o) {
+        try {
+            return new ObjectMapper().writeValueAsBytes(o);
+        } catch (JsonProcessingException e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, Object data) {
+        return Serializer.super.serialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
+}
